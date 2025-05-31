4e54359fe167c00568ee2afd69748ef22dcc3e00
Jess <jessgatura@gmail.com>
2025-05-31 09:08:08 +0300

    mod: added new routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,6 @@
 abstract class AppRoutes {
   static const addVisits = '/addVisits';
   static const home = '/home';
+  static const visitDetail = '/visitDetail';
+  static const editVisit = '/editVisit';
 }
